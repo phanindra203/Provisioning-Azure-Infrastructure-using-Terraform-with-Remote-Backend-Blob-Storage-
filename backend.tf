4e24268fded2467eeb4204dd93fa0e7f41defad0
Phanindra Shatagopam <phanindra203@gmail.com>
2026-04-21 22:40:08 +0530

    Remove access_key from backend.tf
    Removed access_key from backend configuration.

---
+++ backend.tf
@@ -0,0 +1,7 @@
+erraform {
+backend "azurerm" {
+   storage_account_name = "examplestoragephani"
+    container_name       = "backend"
+    key                  = "terraform.tfstate"
+    access_key            = "
+}
