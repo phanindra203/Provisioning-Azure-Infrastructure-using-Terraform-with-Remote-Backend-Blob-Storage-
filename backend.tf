60b9531ae77bd091fd7960679caec9e35dd91707
Phanindra Shatagopam <phanindra203@gmail.com>
2026-04-21 22:40:35 +0530

    Fix access_key formatting in backend.tf

--- backend.tf
+++ backend.tf
@@ -3,5 +3,5 @@ backend "azurerm" {
    storage_account_name = "examplestoragephani"
     container_name       = "backend"
     key                  = "terraform.tfstate"
-    access_key            = "
+    access_key            = ""
 }
